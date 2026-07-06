976f93f3b5ab784351175e64cad4cd17e0d99f2e
Maximiliano Alcaide <maximiliano.alcaide@pdgsa.com>
2026-07-06 15:19:19 -0500

    fix: URL correcta del proyecto Supabase (jy no jv)

--- config.js
+++ config.js
@@ -7,6 +7,6 @@
    ============================================================ */
 
 window.APP_CONFIG = {
-  SUPABASE_URL: 'https://epxwcuiujvguyjimyszh.supabase.co',
+  SUPABASE_URL: 'https://epxwcuiujyguyjimyszh.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVweHdjdWl1anlndXlqaW15c3poIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzNjEzMDgsImV4cCI6MjA5ODkzNzMwOH0.YMitrKOHJj7Z4n_eEgzqu4ppngUyeZkihaymtM2lb-E'
 };
